5a3fc7ebe9b98aa5699dcf0451486adec029d1fc
禹璐 <yulu@chemi.ren>
2021-03-27 09:40:56 +0800

    ui update

--- components/hero-post.js
+++ components/hero-post.js
@@ -13,13 +13,13 @@ export default function HeroPost({
 }) {
   return (
     <section>
-      <div className="mb-8 md:mb-16">
+      {coverImage && <div className="mb-8 md:mb-16">
         <CoverImage
           title={title}
           responsiveImage={coverImage.responsiveImage}
           slug={slug}
         />
-      </div>
+      </div>}
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
