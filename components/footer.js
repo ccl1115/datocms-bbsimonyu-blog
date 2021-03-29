3a470b09e4cdf2150e96e713fe0ab5098110453b
禹璐 <yulu@chemi.ren>
2021-03-29 10:29:37 +0800

    add links to footer

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,22 @@
-import Container from './container'
+import Container from "./container";
 
 export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
+        <div className="py-28 flex flex-col lg:flex-row gap-x-10 items-center">
+          <p class="text-blue-800 shadow-sm px-3 py-1 bg-white rounded-sm hover:shadow-lg transition-shadow">
+            <a href="https://portfolio.bbsimonyu.com">My portfolio</a>
+          </p>
+          <p class="text-blue-800 shadow-sm px-3 py-1 bg-white rounded-sm hover:shadow-lg transition-shadow">
+            <a href="https://thumb.bbsimonyu.com">Thumb Launcher App</a>
+          </p>
+          <p class="text-blue-800 shadow-sm px-3 py-1 bg-white rounded-sm hover:shadow-lg transition-shadow">
+            <a href="https://github.com/ccl1115">@Github</a>
+          </p>
           <p>© 2021 All rights reserved by Simon Yu</p>
         </div>
       </Container>
     </footer>
-  )
+  );
 }
